6a7a1046c7c5665918281e1233d03afdb3c928af
rabarbar362 <barbara.nogas@gmail.com>
2018-03-08 14:04:01 +0100

    +check connection feature

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -27,13 +27,29 @@ function personalInfo() {
 }
 
 function businessCard() {
-	info =  'Business Card :' + '\n' +
-			'********' + '\n' +
-			'Imię i nazwisko  : Barbara  Nogaś' + '\n' + 
-			'E-mail : barbara.nogas@gmail.com' + '\n' + 
-			'Website : brak'+ '\n' + 
-			'Grupa dziekańska : KrDUIs 1012' + '\n' + 
-			'Kierunek : Informatyka stosowana' + '\n';
+	info =  'Business Card :' 						+ '\n' +
+			'********' 								+ '\n' +
+			'Imię i nazwisko  : Barbara  Nogaś' 	+ '\n' + 
+			'E-mail : barbara.nogas@gmail.com' 		+ '\n' + 
+			'Website : brak'						+ '\n' + 
+			'Grupa dziekańska : KrDUIs 1012' 		+ '\n' + 
+			'Kierunek : Informatyka stosowana' 		+ '\n';
 
 	navigator.notification.alert(info);
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    navigator.notification.alert('Connection type: ', states[networkState], 'Check connection', 'OK!');
 }
