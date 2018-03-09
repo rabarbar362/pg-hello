92dc5faa58b6dc5d923ea28d2942ea53956b16fa
rabarbar362 <barbara.nogas@gmail.com>
2018-03-09 18:31:44 +0100

    changed appearance

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -13,26 +13,26 @@ function alertDismissed() {
 function deviceInfo() {
 
 	info = 	'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
+			'Device Cordova : '  + device.cordova	+ '\n' + 
+			'Device Platform: ' + device.platform 	+ '\n' + 
 			'Device UUID    : '     + device.uuid     + '\n' + 
 			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
+			'Device Version : '  + device.version  	+ '\n';
 
-	navigator.notification.alert('Hi, I am your smartphone :)'+ '\n' + info, alertDismissed, 'Info:', 'OK!');
+	navigator.notification.alert('\n' + info, alertDismissed, 'Hi, I am your smartphone :)', 'OK!');
 	
 }
 
 function businessCard() {
-	info =  'Business Card :' 						+ '\n' +
-			'********' 								+ '\n' +
-			'Imię i nazwisko  : Barbara  Nogaś' 	+ '\n' + 
-			'E-mail : barbara.nogas@gmail.com' 		+ '\n' + 
-			'Website : brak'						+ '\n' + 
-			'Grupa dziekańska : KrDUIs 1012' 		+ '\n' + 
-			'Kierunek : Informatyka stosowana' 		+ '\n';
+	info = 	'Name  : Barbara  Nogaś' 					+ '\n' + 
+			'E-mail : barbara.nogas@gmail.com' 			+ '\n' + 
+			'Website : https://github.com/rabarbar362'	+ '\n' + 
+			'Field of study : Applied Informatics' 		+ '\n' +
+			'Deans group number : KrDUIs 1012' 			+ '\n'; 
 
-	navigator.notification.alert(info);
+	
+	
+			navigator.notification.alert('\n'+info, alertDismissed, 'About me', 'OK!');
 }
 
 function checkConnection() {
@@ -48,5 +48,5 @@ function checkConnection() {
     states[Connection.CELL]     = 'cell generic connection';
     states[Connection.NONE]     = 'no network connection :(';
  
-    navigator.notification.alert('Connection type: ' + states[networkState], alertDismissed, 'Check connection', 'OK!');
+    navigator.notification.alert('Connection type: '+ '\n' + states[networkState], alertDismissed, 'Check connection', 'OK!');
 }
