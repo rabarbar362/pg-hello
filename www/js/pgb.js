64a0328684d784fa837ccdb94a359093afbbda75
rabarbar362 <barbara.nogas@gmail.com>
2018-03-10 13:46:11 +0100

    added get/watch acceleration (test)

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -50,3 +50,43 @@ function checkConnection() {
  
     navigator.notification.alert('Connection type: '+ '\n' + states[networkState], alertDismissed, 'Check connection', 'OK!');
 }
+
+function getAcceleration() {
+	navigator.accelerometer.getCurrentAcceleration(
+	   accelerometerSuccess, accelerometerError);
+ 
+	function accelerometerSuccess(acceleration) {
+	   alert('Acceleration X: ' + acceleration.x + '\n' +
+		  'Acceleration Y: ' + acceleration.y + '\n' +
+		  'Acceleration Z: ' + acceleration.z + '\n' +
+		  'Timestamp: '      + acceleration.timestamp + '\n');
+	};
+ 
+	function accelerometerError() {
+	   alert('onError!');
+	};
+ }
+ 
+ function watchAcceleration() {
+	var accelerometerOptions = {
+	   frequency: 3000
+	}
+	var watchID = navigator.accelerometer.watchAcceleration(
+	   accelerometerSuccess, accelerometerError, accelerometerOptions);
+ 
+	function accelerometerSuccess(acceleration) {
+	   alert('Acceleration X: ' + acceleration.x + '\n' +
+		  'Acceleration Y: ' + acceleration.y + '\n' +
+		  'Acceleration Z: ' + acceleration.z + '\n' +
+		  'Timestamp: '      + acceleration.timestamp + '\n');
+ 
+	   setTimeout(function() {
+		  navigator.accelerometer.clearWatch(watchID);
+	   }, 10000);
+	};
+ 
+	function accelerometerError() {
+	   alert('onError!');
+	};
+	 
+ }
